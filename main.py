dafa6eb6f64342d6ca9295709dd5ba7e9f1f48ee
yu <peacetomori1118@gmail.com>
2025-02-25 21:19:01 +0900

    chat on terminal

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -12,5 +18,32 @@ class State(TypedDict):
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
 
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
 
 graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
