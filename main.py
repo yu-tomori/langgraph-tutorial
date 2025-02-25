ed4674583d69b973c7d56bcd0b1452ac6bcfc3e7
yu <peacetomori1118@gmail.com>
2025-02-25 21:41:25 +0900

    use tools

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -19,7 +23,7 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
@@ -32,18 +36,3 @@ def stream_graph_updates(user_input: str):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
 
-
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
